2be2b6871e88bb52722e54c5bb56e61ef1045740
wulang <sanliangitch96@gmail.com>
2019-12-14 21:12:38 +0800

    tcp粘包问题

---
+++ src/main/java/com/wulang/nettydemo/netty/tcp/MyServerInitializer.java
@@ -0,0 +1,18 @@
+package com.wulang.nettydemo.netty.tcp;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * @author wulang
+ * @create 2019/12/14/20:14
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new MyServerHandler());
+    }
+}
